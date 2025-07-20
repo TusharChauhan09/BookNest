3da5ad4cd2212071982835b58f008e17eb1784ea
Tushar Chauhan <tusharchauhan0912@gmail.com>
2025-07-20 21:50:41 +0530

    prod..5

--- server/src/index.js
+++ server/src/index.js
@@ -32,7 +32,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client", "dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
